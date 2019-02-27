4b3b2bc233831a3cd3958393f9f0bd4c303eec83
Kazakov Stepan <ksrt12group@gmail.com>
2019-02-28 02:13:42 +0300

    address Wellbeing permissions
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -22,5 +22,5 @@ HAVOC_TARGET_PACKAGE := $(PRODUCT_OUT)/$(HAVOC_VERSION).zip
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(HAVOC_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(HAVOC_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(HAVOC_TARGET_PACKAGE).md5sum
-	$(hide) ./device/xiaomi/gemini/tools/generate_gemini_info.sh $(HAVOC_TARGET_PACKAGE) $(HAVOC_BUILD_DATE)
+	$(hide) ./device/xiaomi/gemini/tools/generate_gemini_info.sh $(HAVOC_TARGET_PACKAGE) $(HAVOC_BUILD_DATE) $(TOP)
 	@echo "Package Complete: $(HAVOC_TARGET_PACKAGE)" >&2
