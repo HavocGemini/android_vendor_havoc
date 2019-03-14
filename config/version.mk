b4eb2aeb2b72f14babfdabe1dab700625adafb67
Kazakov Stepan <ksrt12group@gmail.com>
2019-03-15 00:32:00 +0300

    build in UTC time
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -8,9 +8,9 @@ endif
 TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
 # Set all versions
-HAVOC_BUILD_DATE := $(shell date +%Y%m%d-%H%M)
+HAVOC_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 HAVOC_VERSION := Havoc-OS-v$(HAVOC_BASE_VERSION)-$(HAVOC_BUILD_DATE)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
-HAVOC_DATE := $(shell date +%d-%m-%Y)
+HAVOC_DATE := $(shell date -u +%d-%m-%Y)
 HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_BASE_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(HAVOC_BUILD_DATE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
