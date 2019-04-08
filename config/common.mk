5301f4dba1d7c3746e3719a6f6db068b2601b6c1
Kazakov Stepan <ksrt12group@gmail.com>
2019-04-08 12:04:12 +0300

    calling havoc sepolicy from vendor
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -243,6 +243,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/havoc/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
 
+# Sepolicy
+include device/havoc/sepolicy/qcom/sepolicy.mk
+
 # Version
 include vendor/havoc/config/version.mk
 
